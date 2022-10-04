fa35e798363fa626735d8330d56190f9306803ab
shmon <shmonyajik@yandex.ru>
2022-10-04 16:55:56 +0300

    Добавил присвоение через псевдонимы

--- core/main.py
+++ core/main.py
@@ -1,108 +1,41 @@
-from dataclasses import fields
-from models import Model, Task
-from pydantic import ValidationError
+
+from models import  Task
 import json
 from loguru import logger
 
-logger.add(r"Debug\debug{time}.log", format="{time} | {level} | {message}", level="DEBUG", rotation="10 KB", compression="zip")
 
 
-def get_current_list( importJson, task = None, taskList = [], counter = 0):
-    counter = counter + 1
-    for item in importJson:
-        if 'name' in item.keys():
-            if item['name'] == 'Name':
-                task = Task()
-                task.name = item['value']
-            if item['name'] == 'Siblings':
-                task.siblings = item['value']
-            if item['name'] == 'Mother':
-                task.mother = item['value']
-            if item['name'] == 'Father':
-                task.father = item['value']
-            if item['name'] == 'Siblings by mother':
-                task.siblings_by_mother = item['value']
-            if item['name'] == 'Owner':
-                task.owner = item['value']
-            if item['name'] == 'Species':
-                task.owner_species = item['value']
-            if item['name'] == 'Occupation':
-                task.species_occupation = item['value']
-            if item['name'] == 'Class':
-                task.occupation_class = item['value']
-            if item['name'] == 'Subclass':
-                task.occupation_subclass = item['value']
-        if 'fields' in item.keys():
-            get_current_list(item['fields'], task = task, taskList=taskList, counter=counter)
-    counter = counter - 1
-    if counter == 1:
-        taskList.append(task)
-    return taskList
-   
-        
- 
+def get_tasks(fields, task=Task(), tasks=[], counter=0):
+
+    for field in fields:
+        if 'name' in field.keys():
+            name = field['name']
+            value = field['value']
+            task_field = task.get_name_by_alias(alias = name)
             
-@logger.catch
+            if name == 'Name':
+                task = Task(name=value)
+                tasks.append(task)
+            else:
+                task.__dict__[task_field] = value
+        
+        if 'fields' in field.keys():
+            get_tasks(field['fields'], task, tasks, counter)
+
+    return tasks
+
+
 def main(): 
-   
     try:
         with open('tasks.json', encoding='utf-8' ) as json_data: 
             importJson = json.load(json_data)
-        tasks = Model(**importJson).task
-    except ValidationError as ex:
-        logger.error(ex.json())
+        
     except Exception as ex:
         logger.error(ex)
 
-    DataClassTasks = get_current_list(importJson['task'])
+    DataClassTasks = get_tasks(importJson['task'])
     print(*DataClassTasks, sep='\n\n')
 
-    #region old
-    # try:
-    #     importJson.get('name')
-    #     for task in importJson['task']:
-    #         DataclassTask = Task()
-    #         for relationship in task['fields']:
-    #             if relationship['name'] == 'Name':
-    #                 DataclassTask.name = relationship['value']
-    #             if relationship['name'] == 'Siblings':
-    #                 DataclassTask.siblings = relationship['value']
-    #             if relationship['name'] == 'Owner':
-    #                 DataclassTask.owner = relationship['value']
-    #             if 'fields' in relationship:
-    #                 for fields in relationship['fields']:
-    #                     if fields['name'] == 'Mother':
-    #                         DataclassTask.mother = fields['value']
-    #                     if fields['name'] == 'Father':
-    #                         DataclassTask.father = fields['value']
-    #                     if fields['name'] == 'Spices':
-    #                         DataclassTask.owner_species = fields['value']
-    #                     if 'fields' in fields:
-    #                         for pre_last_fields in fields['fields']:
-    #                             if pre_last_fields['name'] == 'Occupation':
-    #                                 DataclassTask.species_occupation = pre_last_fields['value']
-    #                             if 'fields' in pre_last_fields:
-    #                                 for last_fields in pre_last_fields['fields']:
-    #                                     if last_fields['name'] == 'Class':
-    #                                         DataclassTask.occupation_class = last_fields['value']
-    #                                     if last_fields['name'] == 'Subclass':
-    #                                         DataclassTask.occupation_subclass = last_fields['value']
-    #         DataclassTasks.append(DataclassTask)
-    # except Exception as ex:
-    #     logger.error(ex)
-    #endregion
-    
-    # for item in DataclassTasks:
-    #     print(item, '\n')
-    #     print('\n')
-                                    
-
-                        
-                        
-
-
-
-        
 
 if __name__== '__main__': 
     main()
--- core/models.py
+++ core/models.py
@@ -1,33 +1,28 @@
 
 
-from typing import List, Optional, Union
+from dataclasses import dataclass, field, fields
+from typing import List
 
 from loguru import logger
-from pydantic import BaseModel, root_validator, validator, Field
 
+@dataclass
+class Task():
+    name: str = field(metadata={'alias': 'Name'}, default=None)
+    siblings: bool = field(metadata={'alias': 'Siblings'}, default=None)
+    mother: str = field(metadata={'alias': 'Mother'}, default=None)
+    father: str = field(metadata={'alias': 'Father'}, default=None)
+    siblings_by_mother: List[str] = field(metadata={'alias': 'Siblings by mother'}, default=None)
+    owner: str = field(metadata={'alias': 'Owner'}, default=None)
+    owner_species: str = field(metadata={'alias': 'Species'}, default=None)
+    species_occupation: str = field(metadata={'alias': 'Occupation'}, default=None)
+    occupation_class: str = field(metadata={'alias': 'Class'}, default=None)
+    occupation_subclass: str = field(metadata={'alias': 'Subclass'}, default=None)
 
-class Task(BaseModel):
-    name: str = None
-    siblings: bool = None
-    mother: str = None
-    father: str = None
-    siblingsbymother: Union[str, List[str]] = Field(None, alias='siblings_by_mother')
-    owner: str = None
-    species: str = Field(None, alias='owner_species')
-    occupation: str = Field(None, alias='species_occupation')
-    a_class : str = Field(None, alias='occupation_class')
-    subclass: str = Field(None, alias='occupation_subclass')
-
-   
-    @validator('father')
-    def validate(cls, value):
-        logger.info("Вызвалось")
-        if value == None:
-            logger.info(f"{cls}")
-            value = "False"
-        return value
-
-    class Config:
-        validate_assignment = True
-
-
+    def get_name_by_alias(self, alias):
+        for item in fields(self):
+            print(item.metadata['alias'])
+            if item.metadata['alias'] == alias:
+                return item.name
+        
+        raise ValueError(f'Не удалось найти псевдоним {alias}')
+        
